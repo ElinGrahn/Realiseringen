f5e0ff3008412bec719732c092a4673679dc1f5a
ElinGrahn <elli.grahn@live.se>
2025-12-22 10:01:54 +0100

    Create Main.java

---
+++ 02_DGSweden/src/sdgsweden/Main.java
@@ -0,0 +1,22 @@
+package sdgsweden;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author nikecederlid
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
