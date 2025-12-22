38c328ca354933fba1217bb3520f2dcb16cf7e35
NikeCederlid <ncederlid@gmail.com>
2025-12-22 09:56:53 +0100

    försök

--- SDGSweden/src/Main.java
+++ SDGSweden/src/Main.java
@@ -1,3 +1,8 @@
+package SDGSweden;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
